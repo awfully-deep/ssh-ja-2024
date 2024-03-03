988ebafdac653e7ba2bd81509af59b98daecb2c2
qifei Will <1581880286@qq.com>
2024-03-03 21:08:02 +0800

    完成了 tabbar 的开发

---
+++ pages/user/user.vue
@@ -0,0 +1,19 @@
+<template>
+	<view>
+		User
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
--- unpackage/dist/dev/mp-weixin/app.js
+++ unpackage/dist/dev/mp-weixin/app.js
@@ -2,20 +2,12 @@
 Object.defineProperty(exports, Symbol.toStringTag, { value: "Module" });
 const common_vendor = require("./common/vendor.js");
 if (!Math) {
-  "./pages/index/index.js";
+  "./pages/home/home.js";
+  "./pages/message/message.js";
+  "./pages/order/order.js";
+  "./pages/user/user.js";
 }
-const _sfc_main = {
-  onLaunch: function() {
-    console.warn("当前组件仅支持 uni_modules 目录结构 ，请升级 HBuilderX 到 3.1.0 版本以上！");
-    console.log("App Launch");
-  },
-  onShow: function() {
-    console.log("App Show");
-  },
-  onHide: function() {
-    console.log("App Hide");
-  }
-};
+const _sfc_main = {};
 const App = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["__file", "C:/Users/15818/Desktop/小程序/uni_proj1/App.vue"]]);
 function createApp() {
   const app = common_vendor.createSSRApp(App);
